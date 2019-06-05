b8f563103fc31c609ef38757da5c266076fc8089
jeremy woo <kepungmoin@hotmail.com>
2019-06-05 19:38:24 +1200

    skeleton batch codes

--- Batch/src/BatchApp/Program.cs
+++ Batch/src/BatchApp/Program.cs
@@ -12,11 +12,12 @@ using System.IO;
 namespace BatchApp
 {
     class Program
-    {
-
-        
+    {        
         private const string BatchAccountName = "";
         private const string BatchAccountKey = "";
+
+        private const string BatchAccountUrl = "";
+
 
 
                 // Storage account credentials
@@ -29,10 +30,40 @@ namespace BatchApp
         {
             Console.WriteLine("Hello World!");
 
-
             CloudBlobClient blobClient = CreateCloudBlobClient(StorageAccountName, StorageAccountKey);
+           
+            UploadWorkFiles(blobClient);
 
+            StartBatchTasks();
+        }
 
+        private static void StartBatchTasks()
+        {
+            
+            BatchSharedKeyCredentials cred = new BatchSharedKeyCredentials(BatchAccountUrl, BatchAccountName, BatchAccountKey);
+
+        }
+
+        private static void UploadWorkFiles(CloudBlobClient blobClient) {
+
+            const string inputContainerName = "input";
+            CloudBlobContainer container = blobClient.GetContainerReference(inputContainerName);       container.CreateIfNotExistsAsync().Wait();
+
+              List<string> inputFilePaths = new List<string>
+                {
+                    "taskdata0.txt",
+                    "taskdata1.txt",
+                    "taskdata2.txt"
+                };
+
+                // Upload the data files to Azure Storage. This is the data that will be processed by each of the tasks that are
+                // executed on the compute nodes within the pool.
+                List<ResourceFile> inputFiles = new List<ResourceFile>();
+
+                foreach (string filePath in inputFilePaths)
+                {
+                    inputFiles.Add(UploadFileToContainer(blobClient, inputContainerName, filePath));
+                }
         }
 
          private static CloudBlobClient CreateCloudBlobClient(string storageAccountName, string storageAccountKey)
@@ -47,6 +78,34 @@ namespace BatchApp
             // Create the blob client
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
             return blobClient;
+        }
+
+        private static ResourceFile UploadFileToContainer(CloudBlobClient blobClient, string containerName, string filePath)
+        {
+            Console.WriteLine("Uploading file {0} to container [{1}]...", filePath, containerName);
+
+            string blobName = Path.GetFileName(filePath);
+
+            filePath = Path.Combine(Environment.CurrentDirectory, filePath);
+
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            CloudBlockBlob blobData = container.GetBlockBlobReference(blobName);
+            blobData.UploadFromFileAsync(filePath).Wait();
+
+            // Set the expiry time and permissions for the blob shared access signature. 
+            // In this case, no start time is specified, so the shared access signature 
+            // becomes valid immediately
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(2),
+                Permissions = SharedAccessBlobPermissions.Read
+            };
+
+            // Construct the SAS URL for blob
+            string sasBlobToken = blobData.GetSharedAccessSignature(sasConstraints);
+            string blobSasUri = String.Format("{0}{1}", blobData.Uri, sasBlobToken);
+
+            return ResourceFile.FromUrl(blobSasUri, filePath);
         }
     }
 }
